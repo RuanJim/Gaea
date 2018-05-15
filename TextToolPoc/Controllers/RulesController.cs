e7d7878069c7ca9d9daa6dde8a6f2bee1fc4361d
Jim <jim.ruan@perkinelmer.com>
2018-05-15 11:17:33 +0800

    Add project files.

---
+++ TextToolPoc/Controllers/RulesController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TextToolPoc.DataAccess;
+
+namespace TextToolPoc.Controllers
+{
+    public class RulesController : ApiController
+    {
+        private TextToolContainer db = new TextToolContainer();
+
+        // GET: api/Rules
+        public IQueryable<DataAccess.Rule> GetRules()
+        {
+            return db.Rules;
+        }
+
+        // GET: api/Rules/5
+        [ResponseType(typeof(DataAccess.Rule))]
+        public IHttpActionResult GetRule(int id)
+        {
+            DataAccess.Rule rule = db.Rules.Find(id);
+            if (rule == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rule);
+        }
+
+        // PUT: api/Rules/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutRule(int id, DataAccess.Rule rule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rule.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(rule).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RuleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Rules
+        [ResponseType(typeof(DataAccess.Rule))]
+        public IHttpActionResult PostRule(DataAccess.Rule rule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Rules.Add(rule);
+            db.SaveChanges();
+
+            return Ok(new { success = true, id = rule.Id });
+        }
+
+        // DELETE: api/Rules/5
+        [ResponseType(typeof(DataAccess.Rule))]
+        public IHttpActionResult DeleteRule(int id)
+        {
+            DataAccess.Rule rule = db.Rules.Find(id);
+            if (rule == null)
+            {
+                return NotFound();
+            }
+
+            db.Rules.Remove(rule);
+            db.SaveChanges();
+
+            return Ok(rule);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RuleExists(int id)
+        {
+            return db.Rules.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ TextToolPoc/Controllers/TemplateFieldsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TextToolPoc.DataAccess;
+
+namespace TextToolPoc.Controllers
+{
+    public class TemplateFieldsController : ApiController
+    {
+        private TextToolContainer db = new TextToolContainer();
+
+        // GET: api/TemplateFields
+        public IQueryable<TemplateField> GetTemplateFields()
+        {
+            return db.TemplateFields;
+        }
+
+        // GET: api/TemplateFields/5
+        [ResponseType(typeof(TemplateField))]
+        public IHttpActionResult GetTemplateField(int id)
+        {
+            TemplateField templateField = db.TemplateFields.Find(id);
+            if (templateField == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(templateField);
+        }
+
+        // PUT: api/TemplateFields/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTemplateField(int id, TemplateField templateField)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != templateField.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(templateField).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TemplateFieldExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/TemplateFields
+        [ResponseType(typeof(TemplateField))]
+        public IHttpActionResult PostTemplateField(TemplateField templateField)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.TemplateFields.Add(templateField);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = templateField.Id }, templateField);
+        }
+
+        // DELETE: api/TemplateFields/5
+        [ResponseType(typeof(TemplateField))]
+        public IHttpActionResult DeleteTemplateField(int id)
+        {
+            TemplateField templateField = db.TemplateFields.Include("FieldExpecteds").Include("FieldMatches").First(f => f.Id == id);
+
+            if (templateField == null)
+            {
+                return NotFound();
+            }
+
+            db.FieldExpecteds.RemoveRange(templateField.FieldExpecteds);
+            db.FieldMatches.RemoveRange(templateField.FieldMatches);
+
+            db.TemplateFields.Remove(templateField);
+            db.SaveChanges();
+
+            return Ok(templateField);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TemplateFieldExists(int id)
+        {
+            return db.TemplateFields.Count(e => e.Id == id) > 0;
+        }
+    }
+}
