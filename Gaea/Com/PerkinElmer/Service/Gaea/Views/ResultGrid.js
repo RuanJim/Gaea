98838e8261ffa7d1729e8db90ea6ae782924d031
Jim <jim.ruan@perkinelmer.com>
2018-05-16 10:35:27 +0800

    TG-43 #closed

---
+++ Gaea/Com/PerkinElmer/Service/Gaea/Views/ResultGrid.js
@@ -0,0 +1,51 @@
+﻿Ext.define('Com.PerkinElmer.Service.Gaea.Views.ResultGrid', {
+    extend: 'Ext.grid.Grid',
+    xtype: 'result-grid',
+
+    title: '匹配结果',
+    docked: 'top',
+    height: 400,
+
+    columns: [
+        { text: '编号', dataIndex: 'Id', width: 100 },
+        { text: '名称', dataIndex: 'FieldName', width: 200 },
+        { text: '期望值', dataIndex: 'ExpectedValue', width: 200 }
+    ],
+
+    listeners: {
+        select: function (src, selectedItems, option) {
+            var field = selectedItems[0].getData();
+
+            var filteredData = [];
+
+            Ext.each(field.FieldMatches, function (fieldMatch) {
+                if (fieldMatch.Article.Id === Com.PerkinElmer.Service.Gaea.Globals.SelectedArticle.Id) {
+                    filteredData.push(fieldMatch);
+                }
+            });
+
+            Ext.getCmp("detailGrid").setStore(Ext.create("Ext.data.Store", {
+                fields: [
+                    'Id',
+                    'MatchText',
+                    'MatchScore',
+                    {
+                        name: 'RuleContent',
+                        type: 'string',
+                        mapping: function (data) {
+                            return data.Rule.Content;
+                        }
+                    },
+                    {
+                        name: 'RuleName',
+                        type: 'string',
+                        mapping: function (data) {
+                            return data.Rule.RuleName;
+                        }
+                    }
+                ],
+                data: filteredData
+            }));
+        }
+    }
+});
--- Gaea/app.js
+++ Gaea/app.js
@@ -13,7 +13,8 @@ Ext.application({
         'Com.PerkinElmer.Service.Gaea.Stores.TemplateStore',
         'Com.PerkinElmer.Service.Gaea.Stores.CaseStore',
 
-        'Com.PerkinElmer.Service.Gaea.Views.DetailsGrid'
+        'Com.PerkinElmer.Service.Gaea.Views.DetailsGrid',
+        'Com.PerkinElmer.Service.Gaea.Views.ResultGrid'
     ],
 
     launch: function () {
@@ -448,57 +449,6 @@ Ext.application({
             }]
         };
 
-        var resultGrid = {
-            id: 'resultGrid',
-            xtype: 'grid',
-            title: '匹配结果',
-            docked: 'top',
-            height: 400,
-
-            columns: [
-                { text: '编号', dataIndex: 'Id', width: 100 },
-                { text: '名称', dataIndex: 'FieldName', width: 200 },
-                { text: '期望值', dataIndex: 'ExpectedValue', width: 200 }
-            ],
-
-            listeners: {
-                select: function (src, selectedItems, option) {
-                    var field = selectedItems[0].getData();
-
-                    var filteredData = [];
-
-                    Ext.each(field.FieldMatches, function (fieldMatch) {
-                        if (fieldMatch.Article.Id === Com.PerkinElmer.Service.Gaea.Globals.SelectedArticle.Id) {
-                            filteredData.push(fieldMatch);
-                        }
-                    });
-
-                    Ext.getCmp("detailGrid").setStore(Ext.create("Ext.data.Store", {
-                        fields: [
-                            'Id',
-                            'MatchText',
-                            'MatchScore',
-                            {
-                                name: 'RuleContent',
-                                type: 'string',
-                                mapping: function (data) {
-                                    return data.Rule.Content;
-                                }
-                            },
-                            {
-                                name: 'RuleName',
-                                type: 'string',
-                                mapping: function (data) {
-                                    return data.Rule.RuleName;
-                                }
-                            }
-                        ],
-                        data: filteredData
-                    }));
-                }
-            }
-        };
-
         var panel = {
             xtype: 'panel',
             layout: 'vbox',
@@ -509,7 +459,10 @@ Ext.application({
                     docked: 'right',
                     layout: 'hbox',
                     width: '50%',
-                    items: [resultGrid, {
+                    items: [{
+                        id: 'resultGrid',
+                        xtype: 'result-grid'
+                    }, {
                         id: 'detailGrid',
                         xtype: 'details-grid'
                     }]
